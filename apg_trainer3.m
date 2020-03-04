57e4a7629fb6d3e25189fcc963366d49f64940f8
David Reixach <davreixach@gmail.com>
2020-03-04 22:48:27 +0100

    exp. denoising

---
+++ apg_trainer3.m
@@ -0,0 +1,109 @@
+function [s_curr,s_hat,result,A_h,B_h] = apg_trainer3(Mtb,para,b,s_hat,btest)      
+%% Initialize variables
+b_hat = fft2(Mtb);
+clear Mtb
+
+if nargin > 3   % dictionary is provided
+    [W,~,z] = init_SWZ(para);
+    solveDict = false;
+else
+    [W,s_small,z] = init_SWZ(para);
+    s = dsmall2d(s_small,para);
+    s_hat = fft2(s);
+    solveDict = true;
+end
+
+if nargin < 5
+    btest = b;
+end
+
+%% Save all objective values and timings
+A_h = [];
+B_h=[];    
+v=[];
+y=[];
+result = [];
+for s_i=1:para.N 
+    if strcmp( para.verbose, 'all')||strcmp( para.verbose, 'outer')
+        fprintf('--%d--\t',s_i) 
+    end
+    temp_b_hat = b_hat(:,:,s_i);
+    temp_b = b(:,:,s_i);
+    temp_btest = btest(:,:,s_i); 
+    t_Z = tic;%~~~~!!!
+    [z_hat_si, W_si,z,iterZ] = updateZW_nmapg(z,W,temp_b_hat,s_hat,para,temp_b);
+    W = (1-1/s_i)*W+1/s_i*W_si; 
+    timeZ = toc(t_Z);
+
+    if strcmp( para.verbose, 'all')||strcmp( para.verbose, 'outer')
+        
+        % NNz and CR
+        NNZ = nnz(z);
+        CR = prod(size(temp_b))/NNZ;
+        
+        [vd_hat]= s2d(W_si,s_hat,para); 
+       [recX] = rec_fre(vd_hat,z_hat_si,para);
+		ps = my_psnr(temp_b,recX,para);
+        ps_test = my_psnr(temp_btest,reCX,para);
+        fprintf('Y: no.iter: %d, psnr: %2.2f, nnz: %i, cr: %.2f\t', iterZ,ps,NNZ,CR)
+        
+        result.PSNR_test_i(s_i) = ps_test;
+        result.PSNR_i(s_i) = ps;
+        result.NNZ_i(s_i) = NNZ;
+        result.CR_i(s_i) = CR;
+
+    end
+    
+    result.iter_code(s_i) = iterZ;
+    result.time_code(s_i) = timeZ;
+    
+    %% dic update
+    if solveDict
+        t_D =tic;%~~~~!!!
+        y_hat_si = z2y(W_si,z_hat_si,para);
+        if isempty(A_h)
+            init_or_not = 1;
+        else
+            init_or_not = 2;
+        end
+        if para.gpu ==1
+            [A_h,B_h] = hist_ocsc_gpu(temp_b_hat,y_hat_si, para, A_h,B_h,init_or_not);
+        else
+            [A_h,B_h] = hist_ocsc_cpu(temp_b_hat,y_hat_si, para, A_h,B_h,init_or_not);    
+        end
+        [s,s_hat,v,y,iterD] = updateD_ocsc_warm(para,A_h,B_h,v,y,s_hat);
+        timeD =toc(t_D);
+
+
+        if strcmp( para.verbose, 'all')||strcmp( para.verbose, 'outer')
+
+            vd_hat = s2d(W_si,s_hat,para);
+            [recX] = rec_fre(vd_hat,z_hat_si,para);
+            ps = my_psnr(temp_b,recX,para);
+           fprintf('S: no.iter: %d, psnr: %2.2f\n', iterD,ps)
+
+        end
+        
+        result.iter_dic(s_i) = iterD;
+        result.time_dic(s_i) = timeD;
+        
+    else
+        fprintf('\n')    
+    end  
+end
+
+if solveDict
+    s_curr = d2dsmall(s,para);
+else
+    s_curr = nan;
+end
+
+result.PSNR_i = gather(result.PSNR_i);
+result.PSNR_test_i = gather(result.PSNR_test_i);
+
+result.PSNR_test = mean(result.PSNR_test_i);
+result.PSNR = mean(result.PSNR_i);
+result.NNZ = sum(result.NNZ_i);
+result.CR = mean(result.CR_i);
+
+end
--- denoising.m
+++ denoising.m
@@ -1,108 +1,104 @@
-% %% ECCV 2020 Compression
-% % David Reixach - IRI(CSIC-UPC) - 02.03.2020
-% % Choudhury Compression
-% 
-% %% Initialization
-% 
-% clear all, close all, clc
-% projectStartup('mcsc')
-% pythonStartup   % correct PY/MKL incompatibility
-% 
-% dbstop if error
-% 
-% cd([project,'/SCSC'])
-% 
-% rng('default')
-% 
-% %% Select data
-% % datasetsPath = '/home/david/Modular/Datasets/CVPR20/';
-% datasetsPath = '/home/dreixach/Modular/Datasets/CVPR20/';
-% 
-% exp = 1;
-% 
-% nameCell = {'09_Choudhury_'};
-% 
-% dataCell = {[datasetsPath,'fruit.mat'];
-%             [datasetsPath,'Choudhury_GT.mat']};
-% 
-% data = dataCell{exp};
-% name = nameCell{exp};
-% dataTest = dataCell{1};
-% 
-% load2(data,'S','b')
-% load2(dataTest,'S','btest')
-% 
-% %% set para
-% 
-% F_exp = [3,5,7,9,11];
-% K_exp = [5,15,25,50,100];
-% lmda_exp = [0.001,0.01,0.1,0.5,1,5,10];
-% 
-% Ri = 10;
-% precS = 1;
-% use_gpu = 1;
-% verbose = 'outer';
-% 
-% %% prepare data
-% 
-% b = squeeze(b);
-% btest = squeeze(btest);
-% 
-% %% run experiment
-% 
-% resTraining = [];
-% resTesting = [];
-% 
-% for F = F_exp
-% 
-%     psf_s=[F,F]; 
-%     psf_radius = floor( psf_s/2 );
-%     
-%     padB = padarray(b, [psf_radius, 0], 0, 'both');
-%     padBtest = padarray(btest, [psf_radius, 0], 0, 'both');
-%     
-%     for K = K_exp    
-%         for L = lmda_exp
-% 
-%             PARAtrain = auto_para_apg(Ri,K,psf_s,b,verbose,precS,use_gpu,1e-3,L);
-%             PARAtest = auto_para_apg(Ri,K,psf_s,btest,verbose,precS,use_gpu,1e-3,L);
-% 
-%             if (PARAtrain.precS ==1)
-%                 b = single(b);
-%             end
-%             if (PARAtrain.gpu ==1)
-%                 b = gpuArray(b);
-%             end
-% 
-%             t1 = tic;
-%             [~,s_hat,R_D] = apg_trainer(padB,PARAtrain,b);
-%             td = toc(t1);    
-%             fprintf('\nDone training K: %i! --> Time: %2.2f s\n\n', K, td)
-% 
-%             t2 = tic;
-%             [~,s_hat,R_Z] = apg_trainer(padBtest,PARAtest,btest,s_hat);    
-%             tc = toc(t2);    
-%             fprintf('\nDone testing K: %i! --> Time: %2.2f s, PSNR: %.2f, CR: %.2f\n\n\n', K, tc,R_Z.PSNR,R_Z.CR)
-% 
-%             R_D.L = L;
-%             R_Z.L = L;
-% 
-%             R_D.K = K;
-%             R_Z.K = K;
-% 
-%             R_D.F = F;
-%             R_Z.F = F;
-% 
-%             resTraining = [resTraining, R_D];
-%             resTesting =  [resTesting, R_Z];
-% 
-%         end
-%     end
-% end
-% 
-% %% save
-% 
-% dataPath = [project,'/data/'];
-% 
-% save2([dataPath,name,'TrainResults.mat'],'dataset','resTraining','-noappend')
-% save2([dataPath,name,'TestResults.mat'],'dataset','resTesting','-noappend')
+%% ECCV 2020 Denoising
+% David Reixach - IRI(CSIC-UPC) - 02.03.2020
+% Choudhury Denoising
+
+%% Initialization
+
+clear all, close all, clc
+projectStartup('mcsc')
+pythonStartup   % correct PY/MKL incompatibility
+
+dbstop if error
+
+cd([project,'/SCSC'])
+
+rng('default')
+
+%% Select data
+% datasetsPath = '/home/david/Modular/Datasets/CVPR20/';
+datasetsPath = '/home/dreixach/Modular/Datasets/CVPR20/';
+
+exp = 1;
+
+nameCell = {'09_Choudhury_SCSC_'};
+
+dataCell = {[datasetsPath,'fruit.mat'];
+            [datasetsPath,'Choudhury_noised_2.mat'];
+            [datasetsPath,'Choudhury_GT.mat']};
+
+data = dataCell{exp};
+name = nameCell{exp};
+dataTest1 = dataCell{2};
+dataTest2 = dataCell{3};
+
+load2(data,'S','b')
+load2(dataTest1,'S','btest1')
+load2(dataTest2,'S','btest2')
+
+%% set para
+
+F = 11;
+K = 100;
+L = 1;
+
+Ri = 10;
+precS = 1;
+use_gpu = 1;
+verbose = 'outer';
+
+%% prepare data
+
+b = squeeze(b);
+btest = squeeze(btest);
+
+%% run experiment
+
+resTraining = [];
+resTesting = [];
+
+
+psf_s=[F,F]; 
+psf_radius = floor( psf_s/2 );
+
+padB = padarray(b, [psf_radius, 0], 0, 'both');
+padBtest = padarray(btest, [psf_radius, 0], 0, 'both');
+
+PARAtrain = auto_para_apg(Ri,K,psf_s,b,verbose,precS,use_gpu,1e-3,L);
+PARAtest = auto_para_apg(Ri,K,psf_s,btest,verbose,precS,use_gpu,1e-3,L);
+
+if (PARAtrain.precS ==1)
+    b = single(b);
+end
+if (PARAtrain.gpu ==1)
+    b = gpuArray(b);
+end
+
+t1 = tic;
+[~,s_hat,R_D] = apg_trainer(padB,PARAtrain,b);
+td = toc(t1);    
+fprintf('\nDone training K: %i! --> Time: %2.2f s\n\n', K, td)
+
+t2 = tic;
+[~,s_hat,R_Z] = apg_trainer3(padBtest,PARAtest,btest1,s_hat,btest2);    
+tc = toc(t2);    
+fprintf('\nDone denoising K: %i! --> Time: %2.2f s, PSNR: %.2f, CR: %.2f\n\n\n', K, tc,R_Z.PSNR_test,R_Z.CR)
+
+R_D.L = L;
+R_Z.L = L;
+
+R_D.K = K;
+R_Z.K = K;
+
+R_D.F = F;
+R_Z.F = F;
+
+resTraining = [resTraining, R_D];
+resTesting =  [resTesting, R_Z];
+
+
+%% save
+
+dataPath = [project,'/data/'];
+
+save2([dataPath,name,'TrainResults.mat'],'dataset','resTraining','-noappend')
+save2([dataPath,name,'TestResults.mat'],'dataset','resTesting','-noappend')
